84b0e9c4011d7d824b647c63e87c2cc1881e8c55
Keshvraaj B B <124709806+keshvraaj0924@users.noreply.github.com>
2026-08-29 12:58:43 +0530

    refactor: expose memory-guided policy

--- remem/memory/__init__.py
+++ remem/memory/__init__.py
@@ -2,10 +2,12 @@
 
 from .episode import EpisodeMemoryAttribution, EpisodeMemoryRecorder
 from .ingestion import EpisodeMemoryIngestor, MemoryIngestionResult
+from .policy import MemoryGuidedPolicy
 
 __all__ = [
     "EpisodeMemoryAttribution",
     "EpisodeMemoryIngestor",
     "EpisodeMemoryRecorder",
+    "MemoryGuidedPolicy",
     "MemoryIngestionResult",
 ]
