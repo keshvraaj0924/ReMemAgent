233a6b856456ef3b4fc353dc1f03777e9878e09a
Keshvraaj B B <124709806+keshvraaj0924@users.noreply.github.com>
2026-08-30 19:03:29 +0530

    feat: export verl training batch

--- remem/integrations/__init__.py
+++ remem/integrations/__init__.py
@@ -7,14 +7,16 @@ from .grpo import (
     build_grpo_samples,
     compute_group_relative_advantages,
 )
-from .verl import VerlTrajectory, encode_episode_for_verl
+from .verl import VerlTrainingBatch, VerlTrajectory, build_verl_training_batch, encode_episode_for_verl
 
 __all__ = [
     "GrpoBatch",
     "GrpoSample",
+    "VerlTrainingBatch",
     "VerlTrajectory",
     "build_grpo_batch",
     "build_grpo_samples",
     "compute_group_relative_advantages",
+    "build_verl_training_batch",
     "encode_episode_for_verl",
 ]
