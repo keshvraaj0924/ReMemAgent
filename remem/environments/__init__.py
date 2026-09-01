58bd62909c985e8a194bf122e5c2c3d8e2e8b353
Keshvraaj B B <124709806+keshvraaj0924@users.noreply.github.com>
2026-09-02 04:01:26 +0530

    feat: export environment contract validator

--- remem/environments/__init__.py
+++ remem/environments/__init__.py
@@ -2,6 +2,14 @@
 
 from remem.environments.alfworld import AlfWorldAdapter
 from remem.environments.base import EnvironmentAdapter, StepResult
+from remem.environments.validation import EnvironmentContractReport, validate_environment_contract
 from remem.environments.webshop import WebShopAdapter
 
-__all__ = ["AlfWorldAdapter", "EnvironmentAdapter", "StepResult", "WebShopAdapter"]
+__all__ = [
+    "AlfWorldAdapter",
+    "EnvironmentAdapter",
+    "EnvironmentContractReport",
+    "StepResult",
+    "WebShopAdapter",
+    "validate_environment_contract",
+]
