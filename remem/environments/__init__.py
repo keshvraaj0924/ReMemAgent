b9caa4c9254066c2c620625369bad4c58a3838ad
Keshvraaj B B <124709806+keshvraaj0924@users.noreply.github.com>
2026-09-15 07:01:18 +0530

    feat: add normalized environment package

---
+++ remem/environments/__init__.py
@@ -0,0 +1,5 @@
+"""Normalized environment contracts and benchmark adapters."""
+
+from remem.environments.base import EnvironmentAdapter, StepResult
+
+__all__ = ["EnvironmentAdapter", "StepResult"]
