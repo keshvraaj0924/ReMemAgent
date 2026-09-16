6fffaae6f8ed112e0862744fccf362047b12d363
Keshvraaj B B <124709806+keshvraaj0924@users.noreply.github.com>
2026-09-16 10:59:45 +0530

    feat: export verl reward field contract

--- remem/training/__init__.py
+++ remem/training/__init__.py
@@ -1,11 +1,12 @@
 """Training integration contracts for learned ReMemAgent policies."""
 
 from remem.training.grpo import GrpoRewardConfig, GrpoTrajectory, compute_grpo_reward
-from remem.training.verl_adapter import VerlRewardAdapter
+from remem.training.verl_adapter import VerlRewardAdapter, VerlRewardFields
 
 __all__ = [
     "GrpoRewardConfig",
     "GrpoTrajectory",
     "VerlRewardAdapter",
+    "VerlRewardFields",
     "compute_grpo_reward",
 ]
