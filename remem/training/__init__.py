992b3a1610e89a19f6743d40988058f5d2416d55
Keshvraaj B B <124709806+keshvraaj0924@users.noreply.github.com>
2026-09-16 07:01:20 +0530

    feat: add training integration package

---
+++ remem/training/__init__.py
@@ -0,0 +1,5 @@
+"""Training integration contracts for learned ReMemAgent policies."""
+
+from remem.training.grpo import GrpoRewardConfig, GrpoTrajectory, compute_grpo_reward
+
+__all__ = ["GrpoRewardConfig", "GrpoTrajectory", "compute_grpo_reward"]
